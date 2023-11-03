30a1d3685019f81299c2523c8cb7313bf656d00a
livotieno <livio5261@gmail.com>
2023-11-03 09:29:11 +0300

    add

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  const handleClick = () => {
+    setInCart(!inCart);
+  };
   return (
-    <li className="">
+    // conditional rendering
+    <li className={`${inCart ? "in-cart" : null}`}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">
+        Add to Cart
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleSelectChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          value={selectedCategory}
+          onChange={handleSelectChange}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +22,13 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items.map(
+          (item) =>
+            // conditional rendering/short circuit
+            item.category === selectedCategory && (
+              <Item key={item.id} name={item.name} category={item.category} />
+            )
+        )}
       </ul>
     </div>
   );
